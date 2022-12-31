dbe619e271403d140bbab682cd626c451b8e0b8e
Sebastian_2302 <seba_colipe@hotmail.com>
2022-12-31 16:43:24 -0300

    app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask
+from flask import Flask, jsonify
 
 # Flask constructor takes the name of
 # current module (__name__) as argument.
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "HOLAA"
+	return jsonify({"error":"data"})
 
 # main driver function
 if __name__ == '__main__':
