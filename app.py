aeabcde8f08c91984d67728a1c7cd94c8e304c7a
Sebastian_2302 <seba_colipe@hotmail.com>
2022-12-31 16:39:12 -0300

    app

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
-@app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-	return 'Hello World'
+
+@app.route("/")
+def hello():
+	return "HOLAA"
 
 # main driver function
 if __name__ == '__main__':
